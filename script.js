99883efde61d2c6165a05baa7ce1a744c29c1cc7
Angry3vlibot <myh.balaker@gmail.com>
2022-10-20 23:45:55 +0300

    Fix button logic, finish styling(for now), finish project(for now)

--- script.js
+++ script.js
@@ -9,64 +9,62 @@ let number2 = 0
 let isSecondNumber = false
 let operator = ''
 
-window.onload = textbar.value = null
+window.onload = textbar.value = ''
 
 function add(numb1, numb2) {
-	return Number(numb1)+Number(numb2)
+    return Number(numb1)+Number(numb2)
 }
 function subtract(numb1, numb2) {
-	return(parseFloat(numb1)-parseFloat(numb2))
+    return(parseFloat(numb1)-parseFloat(numb2))
 }
 function multiply(numb1, numb2) {
-	return(parseInt(numb1)*parseInt(numb2))
+    return(parseInt(numb1)*parseInt(numb2))
 }
 function divide(numb1, numb2) {
-	return(parseInt(numb1)/parseInt(numb2))
+	if (numb2 === 0) {
+		return('To infinity and beyond!')
+	}
+    return(parseInt(numb1)/parseInt(numb2))
 }
 function exponent(numb1, numb2){
-	return(parseInt(numb1)**parseInt(numb2))
+    return(parseInt(numb1)**parseInt(numb2))
 }
 function operate(num1, operation, num2) {
-	switch(operation){
-		case '+':
-			textbar.setAttribute('value', add(num1, num2));
-			break
-		case '-':
-			textbar.setAttribute('value', subtract(num1, num2));
-			break
-		case 'x':
-		case '*':
-			textbar.setAttribute('value', multiply(num1, num2));
-			break
-		case '÷':
-		case '/':	
-			textbar.setAttribute('value', divide(num1, num2));
-			break
-		case '^':						
-			textbar.setAttribute('value', exponent(num1, num2));
-			break
-		default:
-			console.log("error");
-			console.log(operator);
-	}
+    switch(operation){
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case 'x':
+        case '*':
+            return multiply(num1, num2);
+        case '÷':
+        case '/':   
+            return divide(num1, num2);
+        case '^':                       
+            return exponent(num1, num2);
+        default:
+            console.log("error");
+            console.log(operator);
+    }
 }
 function writeNumber(){
-	textbar.value += this.innerHTML
+    textbar.value += this.innerHTML
 }
 function operatorSelect(){
-	operator = this.innerHTML
-	number1 = Number(textbar.value)
-	textbar.value = null
+    operator = this.innerHTML
+    number1 = Number(textbar.value)
+    textbar.value = null
 }
 function clearOperator(){
-	textbar.value = null
-	number1 = 0
-	number2 = 0
-	operator = ''
+    textbar.value = null
+    number1 = 0
+    number2 = 0
+    operator = ''
 }
 function equalsOperator(){
-	number2 = Number(textbar.value)
-	textbar.value = operate(number1, operator, number2)
+    number2 = Number(textbar.value)
+    textbar.value = operate(number1, operator, number2)
 }
 numberContainer.forEach(element => element.addEventListener('click', writeNumber))
 operationContainer.forEach(element => element.addEventListener('click', operatorSelect))
